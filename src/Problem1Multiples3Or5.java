4a4c322d8649d8029f350426476433a154e160a8
David971015 <davidgarciagamboa971015@gmail.com>
2024-05-04 02:43:50 -0500

    Rehago el codigo para el problema 1 de project euler

--- src/Problem1Multiples3Or5.java
+++ src/Problem1Multiples3Or5.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Problem1Multiples3Or5 {
     /*Problem 1: Multiples of 3 or 5
     If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
@@ -7,34 +9,23 @@ public class Problem1Multiples3Or5 {
         int test = multiplesOf3Or5(10);
         System.out.println(test);
         System.out.println("Testing");
+
+
     }
 
     public static int multiplesOf3Or5(int number) {
         int sum = 0;
-        int naturalNumber = 1;
-        boolean isTrue = true;
 
-        while (isTrue) {
-            int multiple3 = 3 * naturalNumber;
-            if (multiple3 >= number) {
-                break;
+        for (int i = 0; i < number; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                sum += i;
             }
-            sum += multiple3;
-            naturalNumber++;
-        }
-
-        naturalNumber = 1;
-        while (isTrue) {
-            int multiple5 = 5 * naturalNumber;
-            if (multiple5 >= number) {
-                break;
-            }
-            sum += multiple5;
-            naturalNumber++;
         }
         return sum;
     }
 
 }
+
+
 
 
